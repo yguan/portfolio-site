8fd3a916e0da724d72ea08171d0591d0316e397c
Yong Guan <gyong9@gmail.com>
2013-10-14 01:22:43 -0700

    update version

--- app/js/chart/framework-history.js
+++ app/js/chart/framework-history.js
@@ -16,12 +16,12 @@ define(function (require, exports, module) {
         chart.setBounds(config.xLeftBound, config.yTopBound, config.width - xBound, config.height - yBound)
         xAxisYear = chart.addCategoryAxis('x', ['year']);
         yAxisName = chart.addCategoryAxis('y', 'name');
-        xAxisDuration =chart.addMeasureAxis('x', 'duration');
+        xAxisDuration = chart.addMeasureAxis('x', 'duration');
         chart.assignColor('Used in year', 'orange', 'orange', 1);
         chart.assignColor('Used number of years', '#9500bc', '#9500bc', 1);
         chart.addSeries('Used in year', dimple.plot.bar, [xAxisYear, yAxisName]);
         chart.addSeries('Used number of years', dimple.plot.bubble, [xAxisDuration, yAxisName]);
-        chart.addLegend(config.xLeftBound, 0 , 290, 15, "left");
+        chart.addLegend(config.xLeftBound, 0, 290, 15, "left");
         chart.draw();
     }
 
