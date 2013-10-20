2a104eed7488ab4d359eab3d5746962d2d89788b
Yong Guan <gyong9@gmail.com>
2013-10-20 13:25:07 -0700

    change build to use less to compile css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,34 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
+        less: {
+            development: {
+                files: [
+                    {
+                        expand: true,     // Enable dynamic expansion.
+                        cwd: 'app/less',  // Src matches are relative to this path.
+                        src: ['*.less'],  // Actual pattern(s) to match.
+                        dest: 'app/css',  // Destination path prefix.
+                        ext: '.css'       // Dest filepaths will have this extension.
+                    }
+                ]
+            },
+            production: {
+                options: {
+                    paths: ["app/css"],
+                    yuicompress: true
+                },
+                src: 'app/less/*.less',
+                dest: 'app/css/app-min.css'
+//                files: {
+//                    "path/to/result.css": "path/to/source.less"
+//                }
+            }
+        },
+        watch: {
+            files: "app/less/*",
+            tasks: ["less"]
+        },
         requirejs: {
             dist: {
                 options: {
@@ -12,6 +40,7 @@ module.exports = function (grunt) {
                     mainConfigFile: "app/js/app.js",
                     dir: 'app-built',
                     name: 'app',
+                    optimizeCss: 'none',
                     findNestedDependencies: true,
                     inlineText: true
 //                    stubModules: ['text']
@@ -21,8 +50,12 @@ module.exports = function (grunt) {
                 }
             }
         }
-    })
-    ;
+    });
 
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', 'requirejs']);
 };
