3b62b02fbfb921562a76dacc373a5526251c2243
Yong Guan <gyong9@gmail.com>
2015-03-21 20:04:48 -0700

    update profile

--- app/js/data/frameworks.js
+++ app/js/data/frameworks.js
@@ -14,18 +14,18 @@ define(function (require, exports, module) {
         chartData,
         data = {
             languages: [
-                { name: 'HTML', years: [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014], duration: 11, level: level.veryProficient },
-                { name: 'CSS', years: [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014], duration: 11, level: level.proficient },
-                { name: 'JavaScript', years: [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014], duration: 11, level: level.veryProficient },
-                { name: 'C#', years: [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014], duration: 9, level: level.veryProficient },
-                { name: 'Transact-SQL', years: [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014], duration: 8, level: level.proficient },
+                { name: 'HTML', years: [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015], duration: 12, level: level.veryProficient },
+                { name: 'CSS', years: [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015], duration: 12, level: level.proficient },
+                { name: 'JavaScript', years: [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015], duration: 12, level: level.veryProficient },
+                { name: 'C#', years: [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015], duration: 10, level: level.veryProficient },
+                { name: 'Transact-SQL', years: [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015], duration: 9, level: level.proficient },
                 { name: 'Python', years: [2012, 2013, 2014], duration: 1, level: level.someExperience },
                 { name: 'VB.NET', years: [2006, 2008], duration: 1.5, level: level.someExperience },
                 { name: 'C++', years: [2004, 2005, 2006], duration: 2.5, level: level.proficient },
                 { name: 'Java', years: [2004, 2005, 2006], duration: 2, level: level.proficient }
             ],
             dotNet: [
-                { name: '.NET 4.5', years: [2013, 2014], duration: 1, level: level.proficient },
+                { name: '.NET 4.5', years: [2013, 2014, 2015], duration: 2, level: level.proficient },
                 { name: '.NET 4.0', years: [2011, 2012], duration: 1.5, level: level.proficient },
                 { name: '.NET 3.5', years: [2010, 2011], duration: 0.6, level: level.proficient },
                 { name: '.NET 3.0', years: [2008, 2009, 2010], duration: 1, level: level.proficient },
@@ -45,12 +45,12 @@ define(function (require, exports, module) {
             ],
             database: [
                 { name: 'IndexedDB', years: [2013], duration: 0.5, level: level.someExperience },
-                { name: 'SQL Server 2005', years: [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014], duration: 7, level: level.proficient },
+                { name: 'SQL Server 2005', years: [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015], duration: 8, level: level.proficient },
                 { name: 'SQL Server 2000', years: [2006, 2007], duration: 0.5, level: level.proficient },
                 { name: 'Access 2003', years: [2004, 2006], duration: 0.5, level: level.someExperience }
             ],
             frontEnd: [
-                { name: 'Ext JS 4.1 & 4.2', years: [2013, 2014], duration: 1.3, level: level.proficient },
+                { name: 'Ext JS 4.1, 4.2, 5.0, 5.1', years: [2013, 2014, 2015], duration: 2, level: level.proficient },
                 { name: 'Backbone.js', years: [2011, 2012, 2013], duration: 1.5, level: level.proficient },
                 { name: 'JQuery', years: [2010, 2011, 2012, 2013], duration: 3.5, level: level.veryProficient },
                 { name: 'Sass', years: [2012], duration: 0.3, level: level.proficient },
@@ -70,20 +70,20 @@ define(function (require, exports, module) {
                 { name: 'WatiN', years: [2008, 2009], duration: 0.5, level: level.someExperience }
             ],
             sourceControl: [
-                { name: 'TFS', years: [2011, 2012, 2013, 2014], duration: 3, level: level.proficient },
-                { name: 'Git', years: [2012, 2013, 2014], duration: 1.5, level: level.someExperience },
+                { name: 'TFS', years: [2011, 2012, 2013, 2014, 2015], duration: 4, level: level.proficient },
+                { name: 'Git', years: [2012, 2013, 2014, 2015], duration: 2, level: level.someExperience },
                 { name: 'Subversion', years: [2010, 2011], duration: 1, level: level.someExperience },
                 { name: 'Visual SourceSafe', years: [2009], duration: 0.5, level: level.someExperience },
                 { name: 'TortoiseCVS', years: [2006, 2007, 2008, 2009], duration: 3, level: level.proficient }
             ],
             ci: [
-                { name: 'TFS', years: [ 2013, 2014], duration: 1.3, level: level.someExperience },
+                { name: 'TFS', years: [ 2013, 2014, 2015], duration: 2, level: level.someExperience },
                 { name: 'Team City', years: [2011, 2012], duration: 1.5, level: level.someExperience },
                 { name: 'CruiseControl.NET', years: [2009, 2010], duration: 0.2, level: level.someExperience }
             ],
             ide: [
-                { name: 'Resharper (8, 7, 6, 5, 4, 3)', years: [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014], duration: 8.5, level: level.someExperience },
-                { name: 'Visual Studio.NET 2012', years: [2012, 2013, 2014], duration: 1.5, level: level.proficient },
+                { name: 'Resharper (9, 8, 7, 6, 5, 4, 3)', years: [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015], duration: 9, level: level.someExperience },
+                { name: 'Visual Studio.NET 2012', years: [2012, 2013, 2014, 2015], duration: 2, level: level.proficient },
                 { name: 'Visual Studio.NET 2010', years: [2010, 2011, 2012], duration: 2.5, level: level.proficient },
                 { name: 'Visual Studio.NET 2008', years: [2009, 2010], duration: 1.5, level: level.proficient },
                 { name: 'Visual Studio.NET 2005', years: [2007, 2008, 2009], duration: 2, level: level.proficient },
